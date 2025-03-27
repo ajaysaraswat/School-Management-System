33a11e50d247e5ea50b17a46b7a2770cf3fce625
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-03-27 13:31:26 +0530

    adding new databse 2

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "sql12767194",
+  database: process.env.DB_NAME || "sql12769839",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
